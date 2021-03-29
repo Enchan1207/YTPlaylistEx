cb1e15d490504ba1378d5b3f0824d66a8bc20932
Enchan <enchant0130@gmail.com>
2021-03-29 11:23:56 +0900

    [Update] sign-up logic implementation, add dummy file of API credentials

---
+++ YTPlaylistEx/APICredentials.swift
@@ -0,0 +1,14 @@
+//
+//  APICredentials.swift - API資格情報
+//  YTPlaylistEx
+//
+//  Created by EnchantCode on 2021/03/29.
+//
+
+import YoutubeKit
+
+let API_KEY = "XXXXXX"
+let CLIENT_ID = "XXXXXX"
+let CLIENT_SECRET = "XXXXXX"
+
+let API_CREDENTIAL = YoutubeKit.APICredential(apikey: API_KEY, clientID: CLIENT_ID, clientSecret: CLIENT_SECRET)
--- YTPlaylistEx/Signup Screen/SignUpViewController.swift
+++ YTPlaylistEx/Signup Screen/SignUpViewController.swift
@@ -6,15 +6,26 @@
 //
 
 import UIKit
+import YoutubeKit
 
 class SignUpViewController: UIViewController {
+    
+    private var youtube: YoutubeKit!
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
+        youtube = .init(apiCredential: API_CREDENTIAL, accessCredential: nil)
     }
 
     @IBAction func onTapSignup(_ sender: UIButton) {
-        
+            
+        youtube.authorize(presentViewController: self, scope: [.readwrite]) { (credential) in
+            print(credential)
+            // TODO: persist access credential
+            self.performSegue(withIdentifier: "ref_to_main", sender: nil)
+        } failure: { (error) in
+            print(error)
+        }
     }
 }
