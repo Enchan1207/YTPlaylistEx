479d2ceb6d680df90f11f8e0a2b2a8c49a5fc611
Enchan <enchant0130@gmail.com>
2021-03-29 12:38:40 +0900

    [Update] transition to Main ViewController

--- YTPlaylistEx/Signup Screen/SignUpViewController.swift
+++ YTPlaylistEx/Signup Screen/SignUpViewController.swift
@@ -11,19 +11,30 @@ import YoutubeKit
 class SignUpViewController: UIViewController {
     
     private var youtube: YoutubeKit!
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
         youtube = .init(apiCredential: API_CREDENTIAL, accessCredential: nil)
     }
-
+    
     @IBAction func onTapSignup(_ sender: UIButton) {
-            
         youtube.authorize(presentViewController: self, scope: [.readwrite]) { (credential) in
             print(credential)
             // TODO: persist access credential
-            self.performSegue(withIdentifier: "ref_to_main", sender: nil)
+            
+            // 前の画面に戻る
+            DispatchQueue.main.async { [weak self] in
+                guard let `self` = self else {return}
+                
+                // 遷移
+                let storyboard = UIStoryboard(name: "Main", bundle: nil)
+                guard let vc = storyboard.instantiateInitialViewController() else{
+                    fatalError("Couldn't generate Main.storyboard initial VC.")
+                }
+                vc.modalPresentationStyle = .fullScreen
+                self.present(vc, animated: true, completion: nil)
+            }
         } failure: { (error) in
             print(error)
         }
